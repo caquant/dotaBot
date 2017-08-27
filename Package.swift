729c53f4b36f83a7287d8bc25828fe95efbcea5d
Carlos Quant <cquant@timerazor.com>
2017-08-27 14:02:14 -0400

    Adding AF to build

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "BroBot",
     dependencies: [
-        .Package(url: "https://github.com/Azoy/Sword", majorVersion: 0, minor: 8)
+        .Package(url: "https://github.com/Azoy/Sword", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
     ]
 )
---
+++ Sources/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  BroBot
+//
+//  Created by Carlos Quant on 8/26/17.
+//
+//
+
+import Foundation
